7d43d0754e1821136eb61ac81e751ff055a4ae9a
Your Name <you@example.com>
2020-01-02 23:01:19 -0400

    'secondcommit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
+import { RouterModule } from '@angular/router';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'products', component: ProductsComponent},
+      {path:'shopping-cart', component: ShoppingCartComponent},
+      {path:'checkout', component: CheckOutComponent},
+      {path:'order-success', component: OrderSuccessComponent},
+      {path:'login', component: LoginComponent},
+      {path:'my/orders', component: MyOrdersComponent},
+      {path:'admin/products', component: AdminProductsComponent},
+      {path:'admin/orders', component: AdminOrdersComponent}
+     
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+
+    apiKey: "AIzaSyBeRLtUqfGBimgidzXXOkTc7cnXDCPVHqA",
+    authDomain: "ecommerceshop-35de3.firebaseapp.com",
+    databaseURL: "https://ecommerceshop-35de3.firebaseio.com",
+    projectId: "ecommerceshop-35de3",
+    storageBucket: "ecommerceshop-35de3.appspot.com",
+    messagingSenderId: "234856756978",
+    appId: "1:234856756978:web:dff7f282887595c565826b",
+    measurementId: "G-8SN59RHVCB"
+    }
 };
