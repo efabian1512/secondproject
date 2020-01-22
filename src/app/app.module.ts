06769243ca2b9abca04229dab7175bbd75edb82d
Your Name <you@example.com>
2020-01-21 20:33:54 -0400

    refactoring the checkout code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
 import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ShippingFormComponent } from './shipping-form/shipping-form.component';
 
 
 
@@ -57,6 +58,7 @@ import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-c
     ProductCardComponent,
     ProductQuantityComponent,
     ShoppingCartSummaryComponent,
+    ShippingFormComponent,
     
   ],
   imports: [
--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,44 +1,28 @@
-import { Router } from '@angular/router';
-import { AuthService } from './../auth.service';
-import { OrderService } from './../order.service';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Order } from '../models/order';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy{ 
-  shipping = {};
-  cart:ShoppingCart;
-  cartSusbcription: Subscription;
-  userSusbcription: Subscription;
-  userId;
+export class CheckOutComponent implements OnInit { 
+ 
+  cart$:Observable<ShoppingCart>;
   
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private orderService: OrderService,
-    private cartService: ShoppingCartService){}
+  
+  constructor(private cartService: ShoppingCartService){}
 
   async ngOnInit(){
-     let cart$ = await this.cartService.getCart();
-     this.cartSusbcription = cart$.subscribe(cart => this.cart = cart);
-     this.userSusbcription = this.authService.user$.subscribe(user => this.userId = user.uid);
+      this.cart$ = await this.cartService.getCart();
+     
+     
   }
   
-  async placeOrder() {
-      let order = new Order(this.userId,this.shipping,this.cart);
-      let result = await this.orderService.storeOrder(order);
-      this.router.navigate(['/order-success',result.key])
-    } 
   
-  ngOnDestroy(){
-    this.cartSusbcription.unsubscribe();
-    this.userSusbcription.unsubscribe();
-  }
+  
+  
 }
